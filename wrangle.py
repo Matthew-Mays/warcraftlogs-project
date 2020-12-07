b5ae25b905f5540cb61af4ba13d8f74f1209134d
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-12-07 14:32:05 -0600

    pushing what I have at the moment.

--- wrangle.py
+++ wrangle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # This function will acquire and prepare the data for exploration
 def get_wrangle_bgs_data():
@@ -18,4 +19,20 @@ def get_wrangle_bgs_data():
     df['faction'] = df['faction'].str.lower()
     df['class'] = df['class'].str.lower()
     
-    return df
+    # Set categorical variable in win column to int (1 = True, 0 = False)
+    df['win'] = df['win'].astype(int)
+    
+    # Changing column name 'class' because that is a reserved word. Will change it to 'c_class' (character_class)
+    df = df.rename(columns={'class': 'c_class'})
+
+    # Adding a column that contains the categorical variable for whether or not the faction is horde
+    df['is_horde'] = [1 if faction == 'horde' else 0 for faction in df.faction]
+    
+    # Adding a seperator to the code column for game #s at 10+ for a certain battleground, this will be useful later for exploration.
+    for bg in df.code:
+        if len(str(bg)) == 4:
+            df.loc[(df['code'] == bg), 'code'] = f'{bg[0:2]}_{bg[2:]}'
+
+    train_validate, test = train_test_split(df, test_size=.25)
+    train, validate = train_test_split(train_validate, test_size=.35)
+    return train, validate, test
